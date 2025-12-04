1cfd3c6461b392b80d7dd3e4feafb8d991ed4f13
Haowei Zhang <haowei.zhang@ipp.mpg.de>
2025-12-04 10:00:31 +0100

    Update scripts.

--- kill-vnc.sh
+++ kill-vnc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~
-vncserver -kill :75
+vncserver -kill :79
--- start-vnc.sh
+++ start-vnc.sh
@@ -8,8 +8,8 @@ cd ~
 #resol=1398x1020 # Display mit 1400x1050
 #resol=1598x1010 # Display mit 1600x1200 oder 1680x1050 (Vollbild)
 #resol=1480x942 # x1028
-resol=2200x1200
+resol=2500x1350
 
-vncserver -fp "built-ins" -depth 24 -geometry $resol :75
+vncserver -fp "built-ins" -depth 24 -geometry $resol :79
 
 fbsetroot -solid rgb:00/66/88 &
